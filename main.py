e45b641881dbeebc343fdb3c84ab565fcef03994
Bruno Tougeiro <b.tougeiro@icloud.com>
2022-06-23 19:47:45 +0100

    added venv gitignore and formatted python file

--- main.py
+++ main.py
@@ -3,30 +3,21 @@ import random
 
 numbers_to_sort = 1000
 
-def bubble_sort(list_of_numbers_to_sort):
-    swapped = True
-    while swapped:
-        swapped = False
-
-        for i in range(1, numbers_to_sort):
-            if list_of_numbers_to_sort[i - 1] > list_of_numbers_to_sort[i]:
-                list_of_numbers_to_sort[i], list_of_numbers_to_sort[i - 1] = list_of_numbers_to_sort[i - 1], list_of_numbers_to_sort[i]
-                swapped = True
 
 def main():
     start = time.time()
 
     list_of_random_numbers = []
-
     for _ in range(numbers_to_sort):
         list_of_random_numbers.append(random.randint(0, 100))
 
-    bubble_sort(list_of_random_numbers)
+    list_of_random_numbers.sort()
 
     end = time.time()
     duration = (end - start) * 1000
 
     print(f"{duration}ms")
+
 
 if __name__ == '__main__':
     main()
